0bffa8351dbd58e36503c3f5637895ae1d66a612
carrinson <folacarrinson@gmail.com>
2025-11-03 20:28:54 +0100

    new add v2

--- src/main/java/org/example/simplecrud/Controllers/ApiController.java
+++ src/main/java/org/example/simplecrud/Controllers/ApiController.java
@@ -1,13 +1,49 @@
 package org.example.simplecrud.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.simplecrud.Models.Users;
+import org.example.simplecrud.Repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class ApiControllers {
+public class ApiController {
+
+    @Autowired
+    private UserRepo userRepo;
 
     @GetMapping(value = "/")
     public  String getPage(){
         return "Welcome";
+    }
+
+    @GetMapping(value = "/users")
+    public List<Users> getUsers(){
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody Users user){
+        userRepo.save(user);
+        return "Saved ...";
+    }
+
+    @PutMapping(value = "update/{id}")
+    public String updateUser(@PathVariable int id, @RequestBody Users user){
+        Users updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setOccupation(user.getOccupation());
+        updatedUser.setAge(user.getAge());
+        userRepo.save(updatedUser);
+        return "Updated ...";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable int id){
+        Users deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Deleted user with the id: "+id;
     }
 }
--- src/main/java/org/example/simplecrud/Repositories/UserRepo.java
+++ src/main/java/org/example/simplecrud/Repositories/UserRepo.java
@@ -1,4 +1,10 @@
 package org.example.simplecrud.Repositories;
 
-public interface UserRepo {
+
+
+import org.example.simplecrud.Models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<Users,Integer> {
+
 }
